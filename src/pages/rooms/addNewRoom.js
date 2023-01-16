c091acfdedc66a581150f22a74770e3c43abe723
Prineth Fernando <prinethfernandox@gmail.com>
2023-01-16 19:40:44 +0530

    add image upload ft

--- src/pages/rooms/addNewRoom.js
+++ src/pages/rooms/addNewRoom.js
@@ -3,10 +3,38 @@ import { Box, Breadcrumbs, Container, Link, Typography } from "@mui/material";
 import { DashboardLayout } from "../../components/dashboard-layout";
 import { useRouter } from "next/router";
 import { id } from "date-fns/locale";
+import { useState } from "react";
+import axios from "axios";
 
 const Page = () => {
   const route = useRouter();
   const roomId = route.query.roomId;
+
+  const [file, setFile] = useState();
+  const [caption, setCaption] = useState("");
+  const [image, setImage] = useState("");
+
+  const submit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("image", file);
+    formData.append("caption", caption);
+    formData.append("email", "hirunaputha16@gmail.com");
+    formData.append("password", "password");
+    formData.append("name", "Hiruna hotel16");
+    formData.append("address", "Malabe");
+
+    await axios.post("http://localhost:5000/hotelier/", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+  };
+
+  const getImage = async () => {
+    await axios.get("http://localhost:5000/hotelier/63c50e074e4be311cbc8a86f").then((response) => {
+     setImage(response.data.images)
+    });
+  };
 
   return (
     <>
@@ -23,18 +51,34 @@ const Page = () => {
       >
         <Container maxWidth={false}>
           <Breadcrumbs aria-label="breadcrumb">
-           
-            <Link
-              underline="hover"
-              color="inherit"
-              href="/rooms"
-            >
+            <Link underline="hover" color="inherit" href="/rooms">
               Rooms
             </Link>
             <Typography color="text.primary">Add new room</Typography>
           </Breadcrumbs>
           <Box sx={{ mt: 3 }}>
             <h1>Add new Room</h1>
+
+            <form onSubmit={submit}>
+              <input
+                onChange={(e) => setFile(e.target.files[0])}
+                type="file"
+                accept="image/*"
+              ></input>
+              <input
+                value={caption}
+                onChange={(e) => setCaption(e.target.value)}
+                type="text"
+                placeholder="Caption"
+              ></input>
+              <button type="submit">Submit</button>
+            </form>
+
+            <button onClick={() => getImage()} type="submit">
+              Get image
+            </button>
+
+            <img src={image}></img>
           </Box>
         </Container>
       </Box>
